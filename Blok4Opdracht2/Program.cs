f2e5e657c16e6f63687759592291c3f0572ef713
Klaas Reitsema <k.reitsema@graafschapcollege.nl>
2024-01-23 14:54:38 +0100

    tekstuele verbeteringen.

--- Blok4Opdracht2/Program.cs
+++ Blok4Opdracht2/Program.cs
@@ -1,5 +1,19 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+// See https://aka.ms/new-console-template for more information
+
+//====================================================================================================//
+// Graafschap College - Smart Industry Programmeren                                                   //
+//                                                                                                    //
+// Auteurs: WGRM & RTMA                                                                               //
+// Leerjaar: 1                                                                                        //
+// Blok: 4                                                                                            //
+// Opdrachtnummer: 2                                                                                  //
+//                                                                                                    //      
+// Beschrijving:                                                                                      //
+// Zelf een klasse toevoegen, instance variabelen, lezen en schrijven met publieke getters en setters //
+//====================================================================================================//
+
 // Stap 1: Maak zelf een nieuwe klasse Auto.cs aan. 
 
 // Stap 2: Maak in de klasse Auto.cs de volgende private (!) instance variabelen aan: _merk (tekst), _type (tekst) en _kilometerstand (geheel getal)
@@ -9,20 +23,22 @@
 // Stap 4: Maak de VraagInfo() en ToonInfo() methodes aan in de Auto.cs klasse
 
 // Stap 5: Maak een nieuwe methode Rijden(int aantalKilometers) in de Auto.cs klasse
-// Pas de kilometerstand aan overeenkomend met de invoerparameter 
+    // Pas de kilometerstand aan overeenkomend met de invoerparameter 
 
- 
+
 // Stap 6: In Program.cs maak je met behulp van een for-loop 3 objecten van de klasse Auto.cs aan.
     // Gebruik de VraagInfo() methode om de eigenschappen in te voeren.
     // Voeg het aangemaakte object toe aan een lijst.
 
 
-// Stap 7: Gebruik een for-loop om met elk object in de lijst te Rijden. 
+// Stap 7: Gebruik een foreach-loop om met elk object in de lijst te Rijden. 
     // Druk de naam en het type van de auto af en vraag hoeveel kilometer de auto gereden heeft
-    // Controleer of de gebruiker een geldig getal is (int.TryParse())
-    // Gebruik vervolgens de methode Rijden(int aantalKilometers) om de _kilometerstand van het object aan te passen.
+        // Controleer of de gebruiker een geldig getal invoert (int.TryParse())
+            // Gebruik een loop om de vraag te blijven stellen tot er een geldig antwoord is ingevoerd. (do while)
+        // Gebruik vervolgens de methode Rijden(int aantalKilometers) om de _kilometerstand van het object aan te passen.
 
 // Stap 8: gebruik een foreach-loop om over alle objecten in de lijst te loopen en de ToonInfo() methode van elk object aan te roepen. 
 
 
+using Blok4Opdracht2;
 // Schrijf hieronder je code vanaf stap 6
