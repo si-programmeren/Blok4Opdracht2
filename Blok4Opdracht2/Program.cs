92c85e9b5e26d67d8260f8bc5998526a6906228e
Klaas Reitsema <k.reitsema@graafschapcollege.nl>
2024-01-23 13:31:49 +0100

    wijzigigen

--- Blok4Opdracht2/Program.cs
+++ Blok4Opdracht2/Program.cs
@@ -1,2 +1,29 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// Stap 1: Maak zelf een nieuwe klasse Auto.cs aan. 
+
+// Stap 2: Maak in de klasse Auto.cs de volgende private (!) instance variabelen aan: _merk (tekst), _type (tekst) en _kilometerstand (geheel getal)
+
+// Stap 3: Maak public eigenschappen aan (get en set) waarmee je alle private instance variabelen kunt lezen en schrijven.
+
+// Stap 4: Maak de VraagInfo() en ToonInfo() methodes aan in de Auto.cs klasse
+
+// Stap 5: Maak een nieuwe methode Rijden(int aantalKilometers) in de Auto.cs klasse
+// Pas de kilometerstand aan overeenkomend met de invoerparameter 
+
+ 
+// Stap 6: In Program.cs maak je met behulp van een for-loop 3 objecten van de klasse Auto.cs aan.
+    // Gebruik de VraagInfo() methode om de eigenschappen in te voeren.
+    // Voeg het aangemaakte object toe aan een lijst.
+
+
+// Stap 7: Gebruik een for-loop om met elk object in de lijst te Rijden. 
+    // Druk de naam en het type van de auto af en vraag hoeveel kilometer de auto gereden heeft
+    // Controleer of de gebruiker een geldig getal is (int.TryParse())
+    // Gebruik vervolgens de methode Rijden(int aantalKilometers) om de _kilometerstand van het object aan te passen.
+
+// Stap 8: gebruik een foreach-loop om over alle objecten in de lijst te loopen en de ToonInfo() methode van elk object aan te roepen. 
+
+
+// Schrijf hieronder je code vanaf stap 6
+//
